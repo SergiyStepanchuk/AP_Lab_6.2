a84028e568efeced4288797178d0b8c11cd87d61
SergiyStepanchuk <serhii.stepanchuk.itis.2020@lpnu.ua>
2020-11-11 20:29:48 +0200

    init

---
+++ UnitTest/UnitTest.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Program/main.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest
+{
+	TEST_CLASS(UnitTest)
+	{
+	public:
+
+		TEST_METHOD(met_myrand)
+		{
+			srand(time(NULL));
+			int m = myrand(1, 3);
+			Assert::IsTrue(m == 1 || m == 2 || m == 3, L"Incorrect method myrand!");
+		}
+	};
+}
